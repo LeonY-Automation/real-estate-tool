25e64f18ef01025308cdca41c95b26a50370323c
Leon <leon@example.com>
2026-06-12 20:37:49 -0700

    fix commonjs export

--- api/generate.js
+++ api/generate.js
@@ -12,7 +12,7 @@ function getUsageKey(ip) {
   return `${ip}:${getToday()}`;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
